26e021ad984eab7d8209f105e8f3ccadabd14aec
Victor Hugo R. de Almeida <43188302+almeidaVictorHugo@users.noreply.github.com>
2019-11-17 20:43:09 -0200

    Alterações

--- README.md
+++
@@ -1,3 +0,0 @@
-1. Unzipar design-system. Na pasta extraída, rodar o comando npm install.
-2. Copiar os arquivos restantes e usá-los para substituir os arquivos da pasta src.
-3. Rodar o comando npm start.
---
+++ src/views/Index.jsx
@@ -0,0 +1,47 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import { Button } from "reactstrap";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+// core components
+import IndexNavbar from "components/Navbars/IndexNavbar.jsx";
+import PageHeader from "components/PageHeader/PageHeader.jsx";
+import Footer from "components/Footer/Footer.jsx";
+
+class Index extends React.Component {
+  componentDidMount() {
+    document.body.classList.toggle("index-page");
+  }
+  componentWillUnmount() {
+    document.body.classList.toggle("index-page");
+  }
+  render() {
+    return (
+      <>
+        <IndexNavbar />
+        <div className="wrapper">
+          <PageHeader />
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
+
+export default Index;
